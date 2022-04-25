3ba63b0850793ebaa23d21ba9115c5da2ab96148
PedroDeBos <pedrodebos@gmail.com>
2022-04-25 15:44:29 +0200

    Adding a new variable to the Celegans file, creating a scatterplot

--- R/installingPackages.R
+++ R/installingPackages.R
@@ -5,7 +5,3 @@ install.packages("drc")
 
 #Instaling FS for RMD 2, creating directory trees
 install.packages("fs")
-
-fs::dir_tree(path = "Daur2_Organisation_2.1/Daur2_Original/")
-
-fs::dir_tree(path = here("Daur2_Organisation_2.1/Daur2_Organized/"))
