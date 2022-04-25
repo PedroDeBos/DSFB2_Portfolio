a52b6abe051528ad60755fa51a61b8d0dbc9f721
PedroDeBos <pedrodebos@gmail.com>
2022-04-25 14:51:21 +0200

    Adding an RMD for formal assignment 2

--- R/installingPackages.R
+++ R/installingPackages.R
@@ -1,1 +1,11 @@
+library(here)
+
+#Installing DRC for RMD 1, to properly determine LD50's 
 install.packages("drc")
+
+#Instaling FS for RMD 2, creating directory trees
+install.packages("fs")
+
+fs::dir_tree(path = "Daur2_Organisation_2.1/Daur2_Original/")
+
+fs::dir_tree(path = here("Daur2_Organisation_2.1/Daur2_Organized/"))
