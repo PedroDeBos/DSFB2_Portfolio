4ea690cc7db4cb11d1dcd7764b29d98fdf003c7f
PedroDeBos <pedrodebos@gmail.com>
2022-05-29 10:54:41 +0200

    Added properly written citations, created a references.bib by hand. Updated installingPackages.R

--- R/installingPackages.R
+++ R/installingPackages.R
@@ -12,5 +12,8 @@ install.packages("vitae")
 #Installing pagedown for RMD 3.1, creating a cv
 install.packages("pagedown")
 
+#Installing rbbt for citation
+devtools::install_github("paleolimbot/rbbt")
+
 #Installing available too check for package names
 install.packages("available")
